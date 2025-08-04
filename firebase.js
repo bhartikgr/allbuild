009cad4619226bcfda56634ea93c127b0b9baf39
avinayk <avinaykumar.web@gmail.com>
2025-08-04 13:05:22 +0530

    combackend

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./commounity-8c822-firebase-adminsdk-fbsvc-00697885c2.json"); // 👈 yeh aapko Firebase console se lena hoga
+const serviceAccount = "";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
